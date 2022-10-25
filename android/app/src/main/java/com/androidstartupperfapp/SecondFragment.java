1e126bb445231146276ffe8413cef7058d915aaa
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2022-10-25 17:23:24 +0800

    feat(app): use Android basic activity project example

---
+++ android/app/src/main/java/com/androidstartupperfapp/SecondFragment.java
@@ -0,0 +1,44 @@
+package com.androidstartupperfapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.androidstartupperfapp.databinding.SecondFragmentBinding;
+
+public class SecondFragment extends Fragment {
+
+  private SecondFragmentBinding binding;
+
+  @Override
+  public View onCreateView(
+    LayoutInflater inflater, ViewGroup container,
+    Bundle savedInstanceState
+  ) {
+    binding = SecondFragmentBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
+
+  public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    binding.secondButton.setOnClickListener(this::onSecondButtonClick);
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
+
+  private void onSecondButtonClick(View view) {
+    NavHostFragment
+      .findNavController(SecondFragment.this)
+      .navigate(R.id.action_SecondFragment_to_FirstFragment);
+  }
+}
