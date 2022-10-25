2033d1a60bd15e11589497a7e06e8381ed416f90
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2022-10-25 17:23:24 +0800

    feat(app): use Android basic activity project example

---
+++ android/app/src/main/java/com/androidstartupperfapp/FirstFragment.java
@@ -0,0 +1,45 @@
+package com.androidstartupperfapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.androidstartupperfapp.databinding.FirstFragmentBinding;
+
+public class FirstFragment extends Fragment {
+
+  private FirstFragmentBinding binding;
+
+  @Override
+  public View onCreateView(
+    LayoutInflater inflater, ViewGroup container,
+    Bundle savedInstanceState
+  ) {
+    binding = FirstFragmentBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    binding.firstButton.setOnClickListener(this::onFirstButtonClick);
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
+
+  private void onFirstButtonClick(View view) {
+    NavHostFragment
+      .findNavController(FirstFragment.this)
+      .navigate(R.id.action_FirstFragment_to_SecondFragment);
+  }
+}
--- android/app/src/main/java/com/androidstartupperfapp/MainActivity.java
+++ android/app/src/main/java/com/androidstartupperfapp/MainActivity.java
@@ -1,15 +1,67 @@
 package com.androidstartupperfapp;
 
-import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends ReactActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+import com.androidstartupperfapp.databinding.MainActivityBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+  private AppBarConfiguration appBarConfiguration;
+  private MainActivityBinding binding;
+
   @Override
-  protected String getMainComponentName() {
-    return "AndroidStartupPerfApp";
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    binding = MainActivityBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+
+    setSupportActionBar(binding.toolbar);
+
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_main_content_fragment);
+    appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+    binding.fab
+      .setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+      .setAction("Action", null).show());
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public boolean onSupportNavigateUp() {
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_main_content_fragment);
+    return NavigationUI.navigateUp(navController, appBarConfiguration)
+      || super.onSupportNavigateUp();
   }
 }
