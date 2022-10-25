e7001f02f1d2ce05bd044361a6adf339d99b1e60
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2022-10-26 00:48:28 +0800

    feat(react): add as separate activity

--- android/app/src/main/java/com/androidstartupperfapp/SecondFragment.java
+++ android/app/src/main/java/com/androidstartupperfapp/SecondFragment.java
@@ -1,5 +1,6 @@
 package com.androidstartupperfapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,6 +29,7 @@ public class SecondFragment extends Fragment {
     super.onViewCreated(view, savedInstanceState);
 
     binding.secondButton.setOnClickListener(this::onSecondButtonClick);
+    binding.startReactActivityButton.setOnClickListener(this::onStartReactActivityButtonClick);
   }
 
   @Override
@@ -40,5 +42,13 @@ public class SecondFragment extends Fragment {
     NavHostFragment
       .findNavController(SecondFragment.this)
       .navigate(R.id.action_SecondFragment_to_FirstFragment);
+  }
+
+  private void onStartReactActivityButtonClick(View view) {
+    // In a fragment, we can't use the current activity (MainActivity) class directly.
+    // However, we can use getActivity() to get the job done.
+    // https://stackoverflow.com/a/15478468/11455106
+    Intent intent = new Intent(getActivity(), ReactActivity.class);
+    startActivity(intent);
   }
 }
