9d66c55a1007b5d8237ce0ae042af0e4a328f47d
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2022-11-04 12:11:48 +0800

    feat(app): prevent Activity destruction when Back is pressed at initial screen

--- android/app/src/main/java/com/androidstartupperfapp/MainActivity.java
+++ android/app/src/main/java/com/androidstartupperfapp/MainActivity.java
@@ -57,4 +57,17 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "AndroidStartupPerfApp";
   }
+
+  // Remove this, then observe the difference in the preservation of the UI state
+  // when the app is backgrounded via the hardware back button and then launched again.
+  @Override
+  public void invokeDefaultOnBackPressed() {
+    // Backgrounds the Application such that this Activity isn't destroyed.
+    // Allows the Application to relaunch with this Activity still alive.
+    // Preserves React state thus ensuring that their values are still intact.
+    // https://github.com/facebook/react-native/issues/13775#issuecomment-299147027
+    // https://stackoverflow.com/questions/38368201/react-native-android-movetasktoback
+    // https://stackoverflow.com/questions/39635398/background-a-react-native-android-app-using-back-button/39639286#39639286
+    this.moveTaskToBack(true);
+  }
 }
