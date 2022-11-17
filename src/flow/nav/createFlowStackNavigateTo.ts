78915f5f06d9740cc5a2ea4f25c124083d72171c
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2022-11-17 15:15:35 +0800

    fix(flow): simplify navigation type
    Ref: #36

--- src/flow/nav/createFlowStackNavigateTo.ts
+++ src/flow/nav/createFlowStackNavigateTo.ts
@@ -1,10 +1,10 @@
 import { NavigationProp } from '@react-navigation/native';
 
 import createNavigateTo from '../../navigation/navigateTo/createNavigateTo';
-import { FLOW_STACK_SCREEN_ROUTES, FlowStackParamList, FlowStackRoute } from './FlowStackRoutes';
+import { FLOW_STACK_SCREEN_ROUTES, FlowStackParamList } from './FlowStackRoutes';
 
-const createFlowStackNavigateTo = (navigator: Pick<NavigationProp<FlowStackParamList, FlowStackRoute>, 'navigate'>) => {
-  return createNavigateTo(navigator, FLOW_STACK_SCREEN_ROUTES);
+const createFlowStackNavigateTo = (navigation: Pick<NavigationProp<FlowStackParamList>, 'navigate'>) => {
+  return createNavigateTo(navigation, FLOW_STACK_SCREEN_ROUTES);
 };
 
 export default createFlowStackNavigateTo;
