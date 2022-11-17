eb1ce4c185461cc94eb18fcf1090f5fdbe5e29b7
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2022-11-17 21:27:40 +0800

    refactor(initialroutenamestate): simplify API

--- src/navigation/initialRouteNameState/InitialRouteNameStateContext.ts
+++ src/navigation/initialRouteNameState/InitialRouteNameStateContext.ts
@@ -5,5 +5,7 @@ import { DEFAULT_INITIAL_ROUTE_NAME } from './InitialRouteNameState';
 const InitialRouteNameStateContext = createContext<string | null>(DEFAULT_INITIAL_ROUTE_NAME);
 
 export const getInitialRouteNameStateContext = <InitialRouteName extends string>() => {
-  return InitialRouteNameStateContext as unknown as React.Context<InitialRouteName | null>;
+  return InitialRouteNameStateContext as React.Context<InitialRouteName | null>;
 };
+
+export default InitialRouteNameStateContext;
