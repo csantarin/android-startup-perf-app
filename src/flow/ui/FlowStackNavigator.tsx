aa1054ba5b1493399914bcc593ea2ff261fda11a
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2022-11-17 14:43:04 +0800

    refactor(flow): remove redundant named export

--- src/flow/ui/FlowStackNavigator.tsx
+++ src/flow/ui/FlowStackNavigator.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { FLOW_STACK_SCREEN_ROUTES, FLOW_STACK_SCREENS, INITIAL_FLOW_STACK_SCREEN_ROUTE } from '../nav/FlowStackRoutes';
 import FlowStack from './FlowStack';
 
-export const FlowStackNavigator = () => {
+const FlowStackNavigator = () => {
   return (
     <FlowStack.Navigator initialRouteName={INITIAL_FLOW_STACK_SCREEN_ROUTE}>
       {FLOW_STACK_SCREEN_ROUTES.map((route) => {
