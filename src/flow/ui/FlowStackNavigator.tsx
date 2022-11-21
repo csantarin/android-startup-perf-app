e5ed0b741a6a0068a1d54d712ac396b1e91a7b69
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2022-11-21 11:54:10 +0800

    refactor(navigation): remove v6 code

--- src/flow/ui/FlowStackNavigator.tsx
+++ src/flow/ui/FlowStackNavigator.tsx
@@ -47,44 +47,4 @@ const FlowStackNavigator = ((props: NavigationInjectedProps) => {
 // https://github.com/react-navigation/react-navigation/blob/4.x/packages/core/src/navigators/createNavigator.js#L8-L11
 hoistNonReactStatics(FlowStackNavigator, FlowStackNavigatorBase);
 
-/*
-export const FlowStackNavigator = () => {
-  const { prevInitialRouteName, initialRouteName } = useInitialRouteNameStateContext(InitialRouteNameStateContext);
-
-  // Every time the initial route name changes to a different string, reconstruct the entire thing.
-  // Why? The initial route name will only be set once(!), i.e. during the initial render.
-  // Subsequent changes won't alter the initial route.
-  return (
-    <FlowStack.Navigator>
-      {getFlowStackScreenRoutes(initialRouteName).map((route) => {
-        const screen = FLOW_STACK_SCREENS[route];
-        return (
-          <FlowStack.Screen
-            key={route}
-            name={route} // e.g. "Step1"
-            component={screen} // e.g. Step1Screen
-            options={{
-              headerShown: !routeIsFlowStackPreInitialRouteName(route),
-              // Force the animation behavior for initial route such that
-              // - going back to the pre-initial route is pop
-              // - going forward from initial route is push
-              // - everything else is the navigation framework default, whatever that value is
-              //
-              // The point of this forced animation is to make the transition phase
-              // between the Android fragments and the React screens look consistent and natural.
-              animationTypeForReplace:
-                routeIsFlowStackInitialRouteName(route) || routeIsFlowStackPreInitialRouteName(route)
-                  ? prevInitialRouteName == null && initialRouteName != null
-                    ? 'push'
-                    : 'pop'
-                  : undefined,
-            }}
-          />
-        );
-      })}
-    </FlowStack.Navigator>
-  );
-};
-*/
-
 export default FlowStackNavigator;
